b9e63152bed66374150b58ea30016255754fa644
Lucas <luking122@gmail.com>
2021-02-04 17:25:42 -0300

    Done Version 1.0 ok

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,69 @@
-console.log("Hello Word!")
+$(document).ready(function () {
+
+  $('input').blur(function () {
+
+    if ($(this).val())
+      $(this).addClass('used');
+    else
+      $(this).removeClass('used');
+
+  });
+  $('textarea').blur(function () {
+
+    if ($(this).val())
+      $(this).addClass('used');
+    else
+      $(this).removeClass('used');
+
+  });
+
+
+});
+
+
+
+let linkHome = document.querySelector("#linkHome")
+let linkPort = document.querySelector("#linkPort")
+let linkContact = document.querySelector("#linkContact")
+
+let projetc = document.querySelector("#projects")
+let contact = document.querySelector("#contact")
+
+function offset(el) {
+  var rect = el.getBoundingClientRect()
+ 
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return  rect.top + scrollTop-200
+}
+
+
+
+
+
+window.addEventListener("scroll", function (event) {
+
+  var scroll = this.scrollY
+
+  if (scroll < offset(projetc)) {
+    linkHome.classList.add("open")
+  } else {
+    linkHome.classList.remove("open")
+
+  }
+  if (scroll > offset(projetc) && scroll < offset(contact)) {
+    linkPort.classList.add("open")
+  } else {
+    linkPort.classList.remove("open")
+  }
+
+  if (scroll > offset(contact)) {
+    linkContact.classList.add("open")
+  } else {
+    linkContact.classList.remove("open")
+  }
+})
+
+
+
+
+
